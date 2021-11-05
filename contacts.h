c5b937a55427a3c7c365253433e0a49a7dc74dee
jaypatel1331 <82784261+jaypatel1331@users.noreply.github.com>
2021-11-05 09:04:48 -0700

    Update contacts.h

--- contacts.h
+++ contacts.h
@@ -1,19 +1,8 @@
 // ----------------------------------------------------------------------------
 // DO NOT remove the below 2 lines.
-// The below two lines is needed to ensure only one copy
-// of this header file is included when compiling the project.
-// (it is referred to as "safeguarding")
-// You will learn about this in C++ (OOP244/BTP200)
-// For now, simply leave this code in!
 #ifndef CONTACTS_H_
 #define CONTACTS_H_
 // ---------------------------------------------------------------------------
-
-// +-------------------------------------------------+
-// | NOTE:  Copy/Paste your Assignment-2 Milestone-3 |
-// |        contents below...                        |
-// +-------------------------------------------------+
-
 
 // Structure type Name declaration (Milestone 1)
 struct Name
